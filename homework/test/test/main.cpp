8eb6c84cb3310cae1e51604c4251ad2179525b7f
yyhaos <35396510+yyhaos@users.noreply.github.com>
2018-12-04 17:51:30 +0800

    12.4

---
+++ homework/test/test/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class A{
+private:
+    int x;
+public:
+
+    A(int xx):x(xx){}
+    A operator+(const A& b)const
+    {
+        return A(this->x + b.x);
+    }
+    friend ostream& operator<<(ostream& os,A &a);
+};
+
+ostream& operator<<(ostream& os,A &a)
+{
+    os << a.x;
+    return os;
+}
+int main(void)
+{
+    A objA1(1), objA2(2);
+    //cout<<endl<<"begin"<<endl;
+    cout<<objA1<<objA2<<endl;
+    cout<<objA1+objA2<<endl;
+    cin.get();
+    return 0;
+}
